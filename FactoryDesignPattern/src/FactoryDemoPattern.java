7162c28d949d760e6636fe7c40deb6a42d7f1187
IT20636524 <IT20636524@my.sliit.lk>
2023-03-08 15:35:45 +0530

    FactoryDesignPattern

--- FactoryDesignPattern/src/FactoryDemoPattern.java
+++ FactoryDesignPattern/src/FactoryDemoPattern.java
@@ -1,5 +1,8 @@
 public class FactoryDemoPattern {
     public static void main(String[] args) {
+        PhoneFactory phoneFactory=new PhoneFactory();
 
+        Phone myPhone = phoneFactory.getPhone("Low Price");
+        myPhone.showSpec();
     }
 }
