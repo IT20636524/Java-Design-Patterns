3800eb87d030653fd0bcd1c7c31bd4caa5c440cb
PrathibhaSathyanjalee <prathibhasathyanjalee@gmail.com>
2024-11-22 21:28:53 +0530

    Builder Pattern

--- BuilderDesignPattern/src/AddEmployee.java
+++ BuilderDesignPattern/src/AddEmployee.java
@@ -2,5 +2,9 @@ public class AddEmployee {
     public static void main(String[] args) {
         Employee employee = new Employee("John", 25, "123 Main St", "123456", "Manager");
         System.out.println(employee);
+
+        //When don't have all the parameters required by the constructor
+        Employee x = new EmployeeBuilder().setName("Prathibha").setAge(25).setAddress("Balangoda").getEmployee();
+        System.out.println(x);
     }
 }
